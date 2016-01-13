d20885e142c7319c94ffea0acb5bfbe41f65d587
modmuss50 <modmuss50@gmail.com>
2016-01-13 13:27:56 +0000

    Fixes TechRebonrn class not found

--- src/main/java/reborncore/common/multiblock/MultiblockTileEntityBase.java
+++ src/main/java/reborncore/common/multiblock/MultiblockTileEntityBase.java
@@ -1,11 +1,11 @@
 package reborncore.common.multiblock;
 
-import net.minecraft.client.renderer.texture.ITickable;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
 import net.minecraft.world.chunk.IChunkProvider;
 
 import java.util.ArrayList;
