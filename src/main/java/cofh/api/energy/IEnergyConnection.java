44018ed143ead484433209f837cb93c21222a4d1
modmuss50 <modmuss50@gmail.com>
2016-02-10 11:12:45 +0000

    Update to use official RF API

--- src/main/java/cofh/api/energy/IEnergyConnection.java
+++ src/main/java/cofh/api/energy/IEnergyConnection.java
@@ -1,21 +1,22 @@
 package cofh.api.energy;
 
-
 import net.minecraft.util.EnumFacing;
+
 
 /**
  * Implement this interface on TileEntities which should connect to energy transportation blocks. This is intended for blocks which generate energy but do not
  * accept it; otherwise just use IEnergyHandler.
  * <p>
- * Note that {@link cofh.api.energy.IEnergyHandler} is an extension of this.
+ * Note that {@link IEnergyHandler} is an extension of this.
  *
  * @author King Lemming
+ *
  */
 public interface IEnergyConnection {
 
-    /**
-     * Returns TRUE if the TileEntity can connect on a given side.
-     */
-    boolean canConnectEnergy(EnumFacing from);
+	/**
+	 * Returns TRUE if the TileEntity can connect on a given side.
+	 */
+	boolean canConnectEnergy(EnumFacing from);
 
 }
