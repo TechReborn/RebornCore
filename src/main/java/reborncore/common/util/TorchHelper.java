cca9746a8582b7586cbed41dcf6e61a431c045c1
gigabit101 <thegigabit101@gmail.com>
2016-03-01 16:14:12 +0000

    fixed TorchHelper

--- src/main/java/reborncore/common/util/TorchHelper.java
+++ src/main/java/reborncore/common/util/TorchHelper.java
@@ -6,35 +6,39 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 import net.minecraftforge.event.ForgeEventFactory;
 
-public class TorchHelper {
-
-    public static boolean placeTorch(ItemStack stack, EntityPlayer player,
-                                     World world, int x, int y, int z, int side, float xOffset,
-                                     float yOffset, float zOffset) {
-        for (int i = 0; i < player.inventory.mainInventory.length; i++) {
+public class TorchHelper 
+{
+    public static boolean placeTorch(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumFacing side, float xOffset, float yOffset, float zOffset) 
+    {
+        for (int i = 0; i < player.inventory.mainInventory.length; i++) 
+        {
             ItemStack torchStack = player.inventory.mainInventory[i];
-            if (torchStack == null
-                    || !torchStack.getUnlocalizedName().toLowerCase()
-                    .contains("torch"))
+            if (torchStack == null || !torchStack.getUnlocalizedName().toLowerCase().contains("torch"))
                 continue;
             Item item = torchStack.getItem();
             if (!(item instanceof ItemBlock))
                 continue;
             int oldMeta = torchStack.getItemDamage();
             int oldSize = torchStack.stackSize;
-            boolean result = Blocks.torch.canPlaceBlockAt(world, new BlockPos(x, y, z));
-            if (player.capabilities.isCreativeMode) {
+            boolean result = torchStack.onItemUse(player, world, pos, side, xOffset, yOffset, zOffset);
+            if (player.capabilities.isCreativeMode) 
+            {
                 torchStack.setItemDamage(oldMeta);
                 torchStack.stackSize = oldSize;
-            } else if (torchStack.stackSize <= 0) {
+            } 
+            else if (torchStack.stackSize <= 0) 
+            {
                 ForgeEventFactory.onPlayerDestroyItem(player, torchStack);
                 player.inventory.mainInventory[i] = null;
             }
             if (result)
+            {
                 return true;
+            }
         }
         return false;
     }
