c9ea75e17d3c2821d3d7c678f69bddc1d50ce75c
modmuss50 <modmuss50@gmail.com>
2016-03-22 20:03:22 +0000

    Fix machines being able to run without power

--- src/main/java/reborncore/common/powerSystem/TilePowerAcceptor.java
+++ src/main/java/reborncore/common/powerSystem/TilePowerAcceptor.java
@@ -227,6 +227,9 @@ public abstract class TilePowerAcceptor extends RFProviderTile implements
 
     @Override
     public double useEnergy(double extract, boolean simulate) {
+        if(extract > energy){
+            return 0;
+        }
         if (!simulate) {
             setEnergy(energy - extract);
         }
