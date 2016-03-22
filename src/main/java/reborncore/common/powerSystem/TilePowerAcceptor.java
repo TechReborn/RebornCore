1f7733635a77989f1043866285dd5378504bcda6
modmuss50 <modmuss50@gmail.com>
2016-03-22 20:02:35 +0000

    Fix Machines not using power

--- src/main/java/reborncore/common/powerSystem/TilePowerAcceptor.java
+++ src/main/java/reborncore/common/powerSystem/TilePowerAcceptor.java
@@ -227,7 +227,6 @@ public abstract class TilePowerAcceptor extends RFProviderTile implements
 
     @Override
     public double useEnergy(double extract, boolean simulate) {
-        extract = Math.min(getMaxOutput(), Math.min(extract, energy));
         if (!simulate) {
             setEnergy(energy - extract);
         }
