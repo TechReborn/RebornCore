601c4ebcaf8056ed716ec12d929887a87dc08954
modmuss50 <modmuss50@gmail.com>
2016-04-13 13:46:05 +0100

    Update forge and fix bucket issues

--- src/main/java/reborncore/common/util/BucketHandler.java
+++ src/main/java/reborncore/common/util/BucketHandler.java
@@ -1,11 +1,14 @@
 package reborncore.common.util;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.FillBucketEvent;
+import net.minecraftforge.fluids.BlockFluidBase;
+import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -16,7 +19,7 @@ public class BucketHandler
 {
 
 	public static BucketHandler INSTANCE = new BucketHandler();
-	public Map<IBlockState, Item> buckets = new HashMap<IBlockState, Item>();
+	public Map<Fluid, Item> buckets = new HashMap<>();
 
 	private BucketHandler()
 	{
@@ -30,29 +33,26 @@ public class BucketHandler
 		if (result == null)
 			return;
 
+		event.setFilledBucket(result);
 		event.setResult(Event.Result.ALLOW);
 	}
 
 	private ItemStack fillCustomBucket(World world, RayTraceResult pos)
 	{
-		if (world.getBlockState(pos.getBlockPos()) != null)
-		{
-			IBlockState state = world.getBlockState(pos.getBlockPos());
-
-			Item bucket = buckets.get(state);
-
-			if (bucket != null)
-			{
-				world.setBlockToAir(pos.getBlockPos());
-				return new ItemStack(bucket);
-			} else
-			{
-				return null;
+		if (pos != null && pos.getBlockPos() != null && world.getBlockState(pos.getBlockPos()) != null) {
+			Block block = world.getBlockState(pos.getBlockPos()).getBlock();
+			if(block instanceof BlockFluidBase){
+				Fluid fluid = ((BlockFluidBase) block).getFluid();
+				if(buckets.containsKey(fluid)){
+					Item bucket = buckets.get(fluid);
+					if (bucket != null) {
+						world.setBlockToAir(pos.getBlockPos());
+						return new ItemStack(bucket, 1);
+					}
+				}
 			}
-		} else
-		{
-			return null;
 		}
+		return null;
 	}
 
 }
