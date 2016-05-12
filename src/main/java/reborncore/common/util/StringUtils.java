2b61a5b08ec1e23e6dab86b8023378d26df6d76b
Prospector <thedoctorsoda@gmail.com>
2016-05-11 18:38:57 -0700

    Added ChatUtils and StringUtils

---
+++ src/main/java/reborncore/common/util/StringUtils.java
@@ -0,0 +1,17 @@
+package reborncore.common.util;
+
+/**
+ * @author Prospector on 11/05/16
+ */
+public class StringUtils
+{
+
+	public static String toFirstCapital(String input) {
+		if (input == null || input.length() == 0) {
+			return input;
+		}
+
+		return input.substring(0, 1).toUpperCase() + input.substring(1);
+	}
+
+}
