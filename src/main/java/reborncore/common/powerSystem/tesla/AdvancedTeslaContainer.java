9825d5c284c1cd1198c47359ea87970eca83c1f3
modmuss50 <modmuss50@gmail.com>
2016-05-14 19:31:28 +0100

    Update Tesla

--- src/main/java/reborncore/common/powerSystem/tesla/AdvancedTeslaContainer.java
+++ src/main/java/reborncore/common/powerSystem/tesla/AdvancedTeslaContainer.java
@@ -1,7 +1,6 @@
 package reborncore.common.powerSystem.tesla;
 
-import net.darkhax.tesla.api.ITeslaHandler;
-import net.darkhax.tesla.api.TeslaContainer;
+import net.darkhax.tesla.api.*;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
@@ -11,7 +10,7 @@ import reborncore.common.powerSystem.TilePowerAcceptor;
 /**
  * Created by modmuss50 on 06/05/2016.
  */
-public class AdvancedTeslaContainer implements ITeslaHandler {
+public class AdvancedTeslaContainer implements ITeslaConsumer, ITeslaHolder, ITeslaProducer {
 
     TilePowerAcceptor tile;
 
@@ -19,17 +18,17 @@ public class AdvancedTeslaContainer implements ITeslaHandler {
         this.tile = tile;
     }
 
-    public AdvancedTeslaContainer(EnumFacing side, NBTBase nbt, TilePowerAcceptor tile) {
+    public AdvancedTeslaContainer(NBTBase nbt, TilePowerAcceptor tile) {
         this.tile = tile;
-        this.readNBT(side, nbt);
+        this.readNBT(nbt);
     }
 
-    public long getStoredPower(EnumFacing side) {
+    public long getStoredPower() {
         return (long)tile.getEnergy() / RebornCoreConfig.euPerRF;
     }
 
-    public long givePower(long tesla, EnumFacing side, boolean simulated) {
-        long acceptedTesla = Math.min(getCapacity(side) - getStoredPower(side), Math.min(getInputRate(), tesla));
+    public long givePower(long tesla, boolean simulated) {
+        long acceptedTesla = Math.min(getCapacity() - getStoredPower(), Math.min(getInputRate(), tesla));
         if(!simulated) {
             tile.setEnergy((long)tile.getEnergy() + (acceptedTesla * RebornCoreConfig.euPerRF));
         }
@@ -37,7 +36,7 @@ public class AdvancedTeslaContainer implements ITeslaHandler {
         return acceptedTesla;
     }
 
-    public long takePower(long tesla, EnumFacing side, boolean simulated) {
+    public long takePower(long tesla,boolean simulated) {
         long removedPower = Math.min((long)tile.getEnergy(), Math.min(getOutputRate(), tesla));
         if(!simulated) {
             tile.setEnergy((long)tile.getEnergy() - (removedPower * RebornCoreConfig.euPerRF));
@@ -46,7 +45,7 @@ public class AdvancedTeslaContainer implements ITeslaHandler {
         return removedPower;
     }
 
-    public long getCapacity(EnumFacing side) {
+    public long getCapacity() {
         return (long)tile.getMaxPower()/ RebornCoreConfig.euPerRF;
     }
 
@@ -58,19 +57,20 @@ public class AdvancedTeslaContainer implements ITeslaHandler {
         return (long)tile.getMaxOutput()/ RebornCoreConfig.euPerRF;
     }
 
-    public NBTBase writeNBT(EnumFacing side) {
+    public NBTBase writeNBT() {
         NBTTagCompound dataTag = new NBTTagCompound();
         return dataTag;
     }
 
-    public void readNBT(EnumFacing side, NBTBase nbt) {
+    public void readNBT(NBTBase nbt) {
     }
 
-    public boolean isInputSide(EnumFacing side) {
+    public boolean isInputSide() {
         return true;
     }
 
-    public boolean isOutputSide(EnumFacing side) {
+    public boolean isOutputSide() {
         return true;
     }
+
 }
