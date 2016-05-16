503b6e9e46a2bd7110df20856ec2176cc2653492
modmuss50 <modmuss50@gmail.com>
2016-05-16 15:10:20 +0100

    Improvements on cable rendering

--- src/main/java/reborncore/client/models/BakedModelUtils.java
+++ src/main/java/reborncore/client/models/BakedModelUtils.java
@@ -17,13 +17,14 @@ public class BakedModelUtils {
                                      ModelRotation modelRotation, TextureAtlasSprite cubeTexture, EnumFacing dir, FaceBakery faceBakery)
     {
         BlockFaceUV uv = new BlockFaceUV(new float[] { (float) cube.getMinX(), (float) cube.getMinY(),
-                (float) cube.getMaxX(), (float) cube.getMaxY() }, 0);
-        face = new BlockPartFace(null, 0, "", uv);
-        if(dir == EnumFacing.NORTH ||  dir == EnumFacing.SOUTH){
-            uv = new BlockFaceUV(new float[] { (float) cube.getMinZ(), (float) cube.getMinY(),
-                    (float) cube.getMaxZ(), (float) cube.getMaxY() }, 0);
-            face = new BlockPartFace(null, 0, "", uv);
-        }
+                (float) cube.getMaxX(), (float) cube.getMaxY() }, 90);
+        face = new BlockPartFace(dir, 0, "", uv);
+//        if(dir == EnumFacing.NORTH ||  dir == EnumFacing.SOUTH){
+//            modelRotation = ModelRotation.X0_Y90;
+//            uv = new BlockFaceUV(new float[] { (float) cube.getMinZ(), (float) cube.getMinY(),
+//                    (float) cube.getMaxZ(), (float) cube.getMaxY() }, 180);
+//            face = new BlockPartFace(dir, 0, "", uv);
+//        }
         list.add(faceBakery.makeBakedQuad(
                 new Vector3f((float) cube.getMinX(), (float) cube.getMinY(), (float) cube.getMinZ()),
                 new Vector3f((float) cube.getMaxX(), (float) cube.getMinY(), (float) cube.getMaxZ()), face, cubeTexture,
