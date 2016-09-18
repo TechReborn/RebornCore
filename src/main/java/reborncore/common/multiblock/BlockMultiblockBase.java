137b5e84852240d316b15142b45d436af2ed88aa
modmuss50 <modmuss50@gmail.com>
2016-09-18 13:07:36 +0100

    Fix block rendering issue

--- src/main/java/reborncore/common/multiblock/BlockMultiblockBase.java
+++ src/main/java/reborncore/common/multiblock/BlockMultiblockBase.java
@@ -1,5 +1,6 @@
 package reborncore.common.multiblock;
 
+import me.modmuss50.jsonDestroyer.api.IOpaqueBlock;
 import net.minecraft.block.material.Material;
 import reborncore.common.BaseTileBlock;
 
@@ -7,7 +8,7 @@ import reborncore.common.BaseTileBlock;
  * Base class for multiblock-capable blocks. This is only a reference implementation
  * and can be safely ignored.
  */
-public abstract class BlockMultiblockBase extends BaseTileBlock
+public abstract class BlockMultiblockBase extends BaseTileBlock implements IOpaqueBlock
 {
 
 	protected BlockMultiblockBase(Material material)
