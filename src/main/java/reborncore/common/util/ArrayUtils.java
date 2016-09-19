f1fd943bb0c4f7b3746baae91694e4f77d60c88b
modmuss50 <modmuss50@gmail.com>
2016-09-19 20:33:23 +0100

    Added ArrayUtils.addAll

--- src/main/java/reborncore/common/util/ArrayUtils.java
+++ src/main/java/reborncore/common/util/ArrayUtils.java
@@ -1,5 +1,7 @@
 package reborncore.common.util;
 
+import java.util.Collection;
+
 /**
  * Created by covers1624 on 3/27/2016.
  */
@@ -11,5 +13,12 @@ public class ArrayUtils {
             copy[i] = array[i].toLowerCase();
         }
         return copy;
+    }
+
+    public static <E> Collection<E> addAll(Collection<E> dest, Collection<? extends E>... src) {
+        for (Collection<? extends E> c : src) {
+            dest.addAll(c);
+        }
+        return dest;
     }
 }
