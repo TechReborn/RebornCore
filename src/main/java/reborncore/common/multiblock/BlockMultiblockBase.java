432a2f839de59f2640939820e940a1bdb8014e62
modmuss50 <modmuss50@gmail.com>
2016-12-05 10:07:41 +0000

    Fix Machine Casing Model

--- src/main/java/reborncore/common/multiblock/BlockMultiblockBase.java
+++ src/main/java/reborncore/common/multiblock/BlockMultiblockBase.java
@@ -8,7 +8,7 @@ import reborncore.common.BaseTileBlock;
  * Base class for multiblock-capable blocks. This is only a reference implementation
  * and can be safely ignored.
  */
-public abstract class BlockMultiblockBase extends BaseTileBlock implements IOpaqueBlock {
+public abstract class BlockMultiblockBase extends BaseTileBlock {
 
 	protected BlockMultiblockBase(Material material) {
 		super(material);
