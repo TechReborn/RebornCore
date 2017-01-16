3efd9b106a3ea4967b084162a69a46d8af40221b
modmuss50 <modmuss50@gmail.com>
2017-01-16 09:48:53 +0000

    Add forge caps based fluid utils.

--- src/main/java/reborncore/common/util/FluidUtils.java
+++ src/main/java/reborncore/common/util/FluidUtils.java
@@ -10,18 +10,65 @@ import net.minecraftforge.fluids.capability.*;
 public class FluidUtils
 {
 
-	//TODO
-	public static boolean drainContainers(net.minecraftforge.fluids.capability.IFluidHandler fluidHandler, IInventory inv, int inputSlot, int outputSlot){
+	//Uses the forge caps system
+	public static boolean drainContainers(FluidTank fluidHandler, IInventory inv, int inputSlot, int outputSlot){
+		ItemStack input = inv.getStackInSlot(inputSlot);
+		ItemStack output = inv.getStackInSlot(outputSlot);
+
+		if (input != null)
+		{
+			FluidStack fluidInContainer = getFluidStackInContainer(input);
+			ItemStack emptyItem = input.getItem().getContainerItem(input);
+			if(input.getItem() instanceof UniversalBucket){
+				emptyItem = ((UniversalBucket) input.getItem()).getEmpty();
+			}
+			if (fluidInContainer != null
+				&& (emptyItem == null || output == null || (output.stackSize < output.getMaxStackSize()
+				&& ItemUtils.isItemEqual(output, emptyItem, true, true))))
+			{
+				int used = fluidHandler.fill(fluidInContainer, false);
+				if (used >= fluidInContainer.amount && fluidHandler.canFill())
+				{
+					fluidHandler.fill(fluidInContainer, true);
+					if (emptyItem != null)
+						if (output == null)
+							inv.setInventorySlotContents(outputSlot, emptyItem);
+						else
+							output.stackSize++;
+					inv.decrStackSize(inputSlot, 1);
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 
-	//TODO
-	public static boolean fillContainers(net.minecraftforge.fluids.capability.IFluidHandler fluidHandler, IInventory inv, int inputSlot, int outputSlot,
+	//Uses the forge caps system
+	public static boolean fillContainers(FluidTank fluidHandler, IInventory inv, int inputSlot, int outputSlot,
 	                                     Fluid fluidToFill){
+		ItemStack input = inv.getStackInSlot(inputSlot);
+		ItemStack output = inv.getStackInSlot(outputSlot);
+		ItemStack filled = getFilledContainer(fluidToFill, input);
+		if (filled != null && (output == null
+			|| (output.stackSize < output.getMaxStackSize() && ItemUtils.isItemEqual(filled, output, true, true))))
+		{
+			FluidStack fluidInContainer = getFluidStackInContainer(filled);
+			FluidStack drain = fluidHandler.drain(fluidInContainer, false);
+			if (drain != null && drain.amount == fluidInContainer.amount)
+			{
+				fluidHandler.drain(fluidInContainer, true);
+				if (output == null)
+					inv.setInventorySlotContents(outputSlot, filled);
+				else
+					output.stackSize++;
+				inv.decrStackSize(inputSlot, 1);
+				return true;
+			}
+		}
 		return false;
 	}
 
-	@Deprecated
+	@Deprecated //Uses the old system
 	public static boolean drainContainers(IFluidHandler fluidHandler, IInventory inv, int inputSlot, int outputSlot)
 	{
 		ItemStack input = inv.getStackInSlot(inputSlot);
@@ -55,7 +102,7 @@ public class FluidUtils
 		return false;
 	}
 
-	@Deprecated
+	@Deprecated //Uses the old system
 	public static boolean fillContainers(IFluidHandler fluidHandler, IInventory inv, int inputSlot, int outputSlot,
 			Fluid fluidToFill)
 	{
