cc8c5e9aad7b5e632ca2196aa5297fa35a6a9128
modmuss50 <modmuss50@gmail.com>
2017-02-26 15:43:14 +0000

    Add a basic mod file

---
+++ src/main/java/teamreborn/reborncore/RebornCore.java
@@ -0,0 +1,7 @@
+package teamreborn.reborncore;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(name = "RebornCore", modid = "reborncore", version = "@MODVERSION@")
+public class RebornCore {
+}
