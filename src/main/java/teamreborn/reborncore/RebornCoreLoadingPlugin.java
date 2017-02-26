f27eb9ec0b0e2201e350eaf33927d9bc383fe090
modmuss50 <modmuss50@gmail.com>
2017-02-26 15:47:45 +0000

    Add the basic shell of a loading plugin

---
+++ src/main/java/teamreborn/reborncore/RebornCoreLoadingPlugin.java
@@ -0,0 +1,38 @@
+package teamreborn.reborncore;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+
+import java.util.Map;
+
+/**
+ * To run this in dev you need to add the following to the *VM* Options in the run config
+ *
+ * -Dfml.coreMods.load=teamreborn.reborncore.RebornCoreLoadingPlugin
+ */
+@IFMLLoadingPlugin.Name("RebornCoreLoadingPlugin")
+public class RebornCoreLoadingPlugin implements IFMLLoadingPlugin {
+	@Override
+	public String[] getASMTransformerClass() {
+		return new String[0];
+	}
+
+	@Override
+	public String getModContainerClass() {
+		return null;
+	}
+
+	@Override
+	public String getSetupClass() {
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data) {
+
+	}
+
+	@Override
+	public String getAccessTransformerClass() {
+		return null;
+	}
+}
