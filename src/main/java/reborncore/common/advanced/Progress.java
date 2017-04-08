79592a7cf8c3534145838a4e5a2701409a5195ad
gigabit101 <thegigabit101@gmail.com>
2017-04-08 20:32:21 +0100

    Added Progress

---
+++ src/main/java/reborncore/common/advanced/Progress.java
@@ -0,0 +1,65 @@
+package reborncore.common.advanced;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.util.INBTSerializable;
+
+/**
+ * Created by Gigabit101 on 08/04/2017.
+ */
+public class Progress implements INBTSerializable<NBTTagCompound>
+{
+    int progress;
+    int maxProgress;
+
+    public Progress(int maxProgress)
+    {
+        this.progress = 0;
+        this.maxProgress = maxProgress;
+    }
+
+    public Progress(int progress, int maxProgress)
+    {
+        this.progress = progress;
+        this.maxProgress = maxProgress;
+    }
+
+    public int getProgress()
+    {
+        return progress;
+    }
+
+    public int getMaxProgress()
+    {
+        return maxProgress;
+    }
+
+    public boolean isProgressCompleate()
+    {
+        if(progress >= getMaxProgress())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public NBTTagCompound serializeNBT()
+    {
+        final NBTTagCompound dataTag = new NBTTagCompound();
+        dataTag.setInteger("progress", this.progress);
+        dataTag.setInteger("maxProgress", this.maxProgress);
+        return dataTag;
+    }
+
+    @Override
+    public void deserializeNBT(NBTTagCompound nbt)
+    {
+        this.progress = nbt.getInteger("progress");
+        this.maxProgress = nbt.getInteger("maxProgress");
+
+        if (this.progress > this.getMaxProgress())
+        {
+            this.progress = this.getMaxProgress();
+        }
+    }
+}
