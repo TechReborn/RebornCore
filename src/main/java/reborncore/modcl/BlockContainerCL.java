bdd1cdda135b7c9f0f4a3734575833c7c5401965
ProfessorProspector <thedoctorsoda@gmail.com>
2017-04-16 14:04:31 -0700

    ModCL changes

---
+++ src/main/java/reborncore/modcl/BlockContainerCL.java
@@ -0,0 +1,45 @@
+package reborncore.modcl;
+
+import net.minecraft.block.BlockContainer;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumBlockRenderType;
+
+/**
+ * Created by Prospector
+ */
+public abstract class BlockContainerCL extends BlockContainer {
+	public ModCL mod;
+	public String name;
+	public Class<? extends TileEntity> tileEntity;
+
+	public BlockContainerCL(ModCL mod, String name, Material material, boolean registerModel, Class<? extends TileEntity> tileEntity) {
+		super(material);
+		setInfo(mod, name, tileEntity);
+		if (registerModel) {
+			mod.blockModelsToRegister.add(this);
+		}
+	}
+
+	public BlockContainerCL(ModCL mod, String name, boolean registerModel, Class<? extends TileEntity> tileEntity) {
+		this(mod, name, Material.ROCK, registerModel, tileEntity);
+	}
+
+	public BlockContainerCL(ModCL mod, String name, Material material, Class<? extends TileEntity> tileEntity) {
+		this(mod, name, material, true, tileEntity);
+	}
+
+	public BlockContainerCL(ModCL mod, String name, Class<? extends TileEntity> tileEntity) {
+		this(mod, name, Material.ROCK, true, tileEntity);
+	}
+
+	private void setInfo(ModCL mod, String name, Class<? extends TileEntity> tileEntity) {
+		this.mod = mod;
+		this.name = name;
+		this.tileEntity = tileEntity;
+		setUnlocalizedName(mod.getPrefix() + name);
+		setRegistryName(mod.getModID(), name);
+		setCreativeTab(mod.getTab());
+	}
+}
--- src/main/java/reborncore/modcl/RegistryCL.java
+++ src/main/java/reborncore/modcl/RegistryCL.java
@@ -14,6 +14,7 @@ public abstract class RegistryCL {
 
 	public LinkedHashMap<String, ItemCL> itemRegistry = new LinkedHashMap<>();
 	public LinkedHashMap<String, BlockCL> blockRegistry = new LinkedHashMap<>();
+	public LinkedHashMap<String, BlockContainerCL> blockContainerRegistry = new LinkedHashMap<>();
 	public HashMap<ItemStack, String> oreEntries = new HashMap<>();
 
 	protected static void register(ItemCL item) {
@@ -23,6 +24,12 @@ public abstract class RegistryCL {
 	protected static void register(BlockCL block) {
 		GameRegistry.register(block);
 		GameRegistry.register(new ItemBlock(block), block.getRegistryName());
+	}
+
+	protected static void register(BlockContainerCL block) {
+		GameRegistry.register(block);
+		GameRegistry.register(new ItemBlock(block), block.getRegistryName());
+		GameRegistry.registerTileEntity(block.tileEntity, "TilePackagerRD");
 	}
 
 	public abstract void init(ModCL mod);
