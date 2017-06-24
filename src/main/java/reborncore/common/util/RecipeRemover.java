7ea70debb33e8bf0c78bdeeb71f0ba103d700150
modmuss50 <modmuss50@gmail.com>
2017-06-24 20:50:42 +0100

    Fix recipe remover

--- src/main/java/reborncore/common/util/RecipeRemover.java
+++ src/main/java/reborncore/common/util/RecipeRemover.java
@@ -28,11 +28,17 @@
 
 package reborncore.common.util;
 
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.ShapedRecipes;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 public class RecipeRemover {
@@ -45,8 +51,7 @@ public class RecipeRemover {
 		for (IRecipe tmpRecipe : CraftingManager.REGISTRY) {
 			ItemStack recipeResult = tmpRecipe.getRecipeOutput();
 			if (ItemStack.areItemStacksEqual(resultItem, recipeResult)) {
-				//TODO 1.12
-				//recipes.remove(i--);
+				removeRecipe(tmpRecipe);
 			}
 		}
 	}
@@ -56,12 +61,66 @@ public class RecipeRemover {
 			if (tmpRecipe instanceof ShapedRecipes) {
 				ShapedRecipes recipe = (ShapedRecipes) tmpRecipe;
 				ItemStack recipeResult = recipe.getRecipeOutput();
-
 				if (ItemStack.areItemStacksEqual(resultItem, recipeResult)) {
-					//TODO 1.12
-					//recipes.remove(i++);
+					removeRecipe(recipe);
 				}
 			}
+		}
+	}
+
+	private static void removeRecipe(IRecipe recipe){
+		ForgeRegistries.RECIPES.register(new BlankRecipe(recipe));
+	}
+
+	//Hax, nothing to see here
+	//Is there a better way to do this?
+	private static class BlankRecipe implements IRecipe {
+
+		IRecipe oldRecipe;
+
+		public BlankRecipe(IRecipe oldRecipe) {
+			this.oldRecipe = oldRecipe;
+		}
+
+		@Override
+		public boolean matches(InventoryCrafting inv, World worldIn) {
+			return false;
+		}
+
+		@Override
+		public ItemStack getCraftingResult(InventoryCrafting inv) {
+			return ItemStack.EMPTY;
+		}
+
+		@Override
+		public boolean canFit(int width, int height) {
+			return false;
+		}
+
+		@Override
+		public ItemStack getRecipeOutput() {
+			return ItemStack.EMPTY;
+		}
+
+		@Override
+		public NonNullList<ItemStack> getRemainingItems(InventoryCrafting inv) {
+			return NonNullList.create();
+		}
+
+		@Override
+		public IRecipe setRegistryName(ResourceLocation name) {
+			return oldRecipe.setRegistryName(name);
+		}
+
+		@Nullable
+		@Override
+		public ResourceLocation getRegistryName() {
+			return oldRecipe.getRegistryName();
+		}
+
+		@Override
+		public Class<IRecipe> getRegistryType() {
+			return oldRecipe.getRegistryType();
 		}
 	}
 }
