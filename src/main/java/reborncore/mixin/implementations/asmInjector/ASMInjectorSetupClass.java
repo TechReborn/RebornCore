8b829cd116dbdb87e7980a0bdeed7f0b3f13eb75
modmuss50 <modmuss50@gmail.com>
2017-06-28 10:16:58 +0100

    Add class path to asm injector

--- src/main/java/reborncore/mixin/implementations/asmInjector/ASMInjectorSetupClass.java
+++ src/main/java/reborncore/mixin/implementations/asmInjector/ASMInjectorSetupClass.java
@@ -1,8 +1,10 @@
 package reborncore.mixin.implementations.asmInjector;
 
+import javassist.LoaderClassPath;
 import org.apache.logging.log4j.LogManager;
 import reborncore.mixin.MixinManager;
 import reborncore.mixin.implementations.prebaker.MixinPrebaker;
+import reborncore.mixin.transformer.MixinTransformer;
 
 /**
  * Used by asm injector to load mixins into a jar at build time
@@ -14,6 +16,7 @@ public class ASMInjectorSetupClass {
 		MixinManager.logger.info("Loading mixin manager");
 		MixinManager.logger.info("Using dummy remapper");
 		MixinManager.mixinRemaper = new MixinPrebaker.DummyRemapper();
+		MixinTransformer.cp.appendClassPath(new LoaderClassPath(ASMInjectorSetupClass.class.getClassLoader()));
 		MixinManager.load();
 	}
 
