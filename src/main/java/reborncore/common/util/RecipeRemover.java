7886864813485aa63b78a923d851097c51c1c25d
modmuss50 <modmuss50@gmail.com>
2017-11-24 14:57:26 +0000

    Improve RecipeRemover

--- src/main/java/reborncore/common/util/RecipeRemover.java
+++ src/main/java/reborncore/common/util/RecipeRemover.java
@@ -28,17 +28,13 @@
 
 package reborncore.common.util;
 
-import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.ShapedRecipes;
-import net.minecraft.util.NonNullList;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.registries.GameData;
+import net.minecraftforge.registries.RegistryManager;
 
-import javax.annotation.Nullable;
 import java.util.List;
 
 public class RecipeRemover {
@@ -69,58 +65,6 @@ public class RecipeRemover {
 	}
 
 	private static void removeRecipe(IRecipe recipe) {
-		ForgeRegistries.RECIPES.register(new BlankRecipe(recipe));
-	}
-
-	//Hax, nothing to see here
-	//Is there a better way to do this?
-	private static class BlankRecipe implements IRecipe {
-
-		IRecipe oldRecipe;
-
-		public BlankRecipe(IRecipe oldRecipe) {
-			this.oldRecipe = oldRecipe;
-		}
-
-		@Override
-		public boolean matches(InventoryCrafting inv, World worldIn) {
-			return false;
-		}
-
-		@Override
-		public ItemStack getCraftingResult(InventoryCrafting inv) {
-			return ItemStack.EMPTY;
-		}
-
-		@Override
-		public boolean canFit(int width, int height) {
-			return false;
-		}
-
-		@Override
-		public ItemStack getRecipeOutput() {
-			return ItemStack.EMPTY;
-		}
-
-		@Override
-		public NonNullList<ItemStack> getRemainingItems(InventoryCrafting inv) {
-			return NonNullList.create();
-		}
-
-		@Override
-		public IRecipe setRegistryName(ResourceLocation name) {
-			return oldRecipe.setRegistryName(name);
-		}
-
-		@Nullable
-		@Override
-		public ResourceLocation getRegistryName() {
-			return oldRecipe.getRegistryName();
-		}
-
-		@Override
-		public Class<IRecipe> getRegistryType() {
-			return oldRecipe.getRegistryType();
-		}
+		RegistryManager.ACTIVE.getRegistry(GameData.RECIPES).remove(recipe.getRegistryName());
 	}
 }
