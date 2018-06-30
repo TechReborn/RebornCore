a17a4269f064f3a146004bbc01b8c34eaef6a368
modmuss50 <modmuss50@gmail.com>
2018-06-30 19:03:14 +0100

    Add back missing method #releaseBuild
    Fixes https://github.com/TechReborn/TechReborn/issues/1562
    Fixes https://github.com/TechReborn/TechReborn/issues/1561

--- src/main/java/reborncore/common/util/ItemUtils.java
+++ src/main/java/reborncore/common/util/ItemUtils.java
@@ -168,5 +168,12 @@ public class ItemUtils {
 		double energy = (double) capEnergy.getEnergyStored();
 		double maxEnergy = (double) capEnergy.getMaxEnergyStored();
 		return energy /  maxEnergy;
-	}	
+	}
+
+	public static boolean isActive(ItemStack stack) {
+		if (!stack.isEmpty() && stack.getTagCompound() != null && stack.getTagCompound().getBoolean("isActive")) {
+			return true;
+		}
+		return false;
+	}
 }
