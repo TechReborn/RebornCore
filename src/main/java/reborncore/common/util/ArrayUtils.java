d75936065b500d3941099342b1edef13a49265f4
modmuss50 <modmuss50@gmail.com>
2019-02-25 18:43:08 +0000

    Fix crash on EclipseJ9

--- src/main/java/reborncore/common/util/ArrayUtils.java
+++ src/main/java/reborncore/common/util/ArrayUtils.java
@@ -38,7 +38,7 @@ public class ArrayUtils {
 	public static String[] arrayToLowercase(String[] array) {
 		String[] copy = new String[array.length];
 		for (int i = 0; i < array.length; i++) {
-			copy[i] = array[i].toLowerCase();
+			copy[i] = array[i].toLowerCase().intern();
 		}
 		return copy;
 	}
