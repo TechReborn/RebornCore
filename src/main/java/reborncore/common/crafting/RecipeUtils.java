0627338f3ee328277c107a71bd5267bc2eef78d0
modmuss50 <modmuss50@gmail.com>
2019-07-29 14:34:13 +0100

    Fix NBT in recipe outputs

--- src/main/java/reborncore/common/crafting/RecipeUtils.java
+++ src/main/java/reborncore/common/crafting/RecipeUtils.java
@@ -30,8 +30,13 @@ package reborncore.common.crafting;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.mojang.datafixers.Dynamic;
+import com.mojang.datafixers.types.JsonOps;
+import net.minecraft.datafixers.NbtOps;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.util.DefaultedList;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
@@ -63,15 +68,19 @@ public class RecipeUtils {
 	private static ItemStack deserializeItem(JsonObject jsonObject){
 		Identifier resourceLocation = new Identifier(JsonHelper.getString(jsonObject, "item"));
 		Item item = Registry.ITEM.get(resourceLocation);
-		if(item == null){
+		if(item == Items.AIR){
 			throw new IllegalStateException(resourceLocation + " did not exist");
 		}
 		int count = 1;
 		if(jsonObject.has("count")){
 			count = JsonHelper.getInt(jsonObject, "count");
 		}
-		//TODO support nbt
-		return new ItemStack(item, count);
+		ItemStack stack = new ItemStack(item, count);
+		if(jsonObject.has("tag")){
+			CompoundTag tag = (CompoundTag) Dynamic.convert(JsonOps.INSTANCE, NbtOps.INSTANCE, jsonObject.get("tag"));
+			stack.setTag(tag);
+		}
+		return stack;
 	}
 
 }
