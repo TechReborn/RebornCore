9198c4fd2bd1cb8c6e07fb899ff4f8b76a592100
modmuss50 <modmuss50@gmail.com>
2019-09-21 12:57:14 +0100

    Comment out game render hooks

--- src/main/java/reborncore/mixin/client/MixinGameRenderer.java
+++ src/main/java/reborncore/mixin/client/MixinGameRenderer.java
@@ -11,15 +11,16 @@ import reborncore.client.ClientChunkManager;
 @Mixin(GameRenderer.class)
 public class MixinGameRenderer {
 
-	@Inject(method = "renderCenter", at = @At(value = "INVOKE", target = "net/minecraft/util/profiler/Profiler.swap(Ljava/lang/String;)V", ordinal = 15))
-	private void renderCenter(float float_1, long long_1, CallbackInfo info) {
-		RebornCoreClient.multiblockRenderEvent.onWorldRenderLast(float_1);
-
-	}
-
-	@Inject(method = "renderCenter", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/debug/DebugRenderer;render(J)V", ordinal = 0))
-	private void renderCenter_2(float float_1, long long_1, CallbackInfo info) {
-		ClientChunkManager.render();
-	}
+	//TODO 1.15 nope
+//	@Inject(method = "renderCenter", at = @At(value = "INVOKE", target = "net/minecraft/util/profiler/Profiler.swap(Ljava/lang/String;)V", ordinal = 15))
+//	private void renderCenter(float float_1, long long_1, CallbackInfo info) {
+//		RebornCoreClient.multiblockRenderEvent.onWorldRenderLast(float_1);
+//
+//	}
+//
+//	@Inject(method = "renderCenter", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/debug/DebugRenderer;render(J)V", ordinal = 0))
+//	private void renderCenter_2(float float_1, long long_1, CallbackInfo info) {
+//		ClientChunkManager.render();
+//	}
 
 }
