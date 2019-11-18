94615c43d98840225c03d05e34b9dd3fc8ad7d03
estebes <estebes@estebes.com>
2019-11-18 18:51:37 +0000

    Small fix.

--- src/main/java/reborncore/common/scriba/TileRegistrationManager.java
+++ src/main/java/reborncore/common/scriba/TileRegistrationManager.java
@@ -27,8 +27,7 @@ public class TileRegistrationManager {
         try (ScanResult result = new ClassGraph().enableClassInfo().enableAnnotationInfo().scan()) {
             ClassInfoList classInfoList = result.getClassesWithAnnotation(TileReference.class.getName());
 
-            classInfoList.stream()
-                    .filter(tileReference -> tileReference.extendsSuperclass(TileEntity.class.getName()))
+            classInfoList.filter(reference -> reference.extendsSuperclass(TileEntity.class.getName()))
                     .forEach(tileReference -> {
                         AnnotationParameterValueList annotationParameters =
                                 tileReference.getAnnotationInfo(TileReference.class.getName()).getParameterValues();
@@ -38,16 +37,21 @@ public class TileRegistrationManager {
 
                         ResourceLocation resourceLocation = new ResourceLocation(resourceDomain, resourcePath);
 
-                        GameRegistry.registerTileEntity(tileReference.getClass().asSubclass(TileEntity.class), resourceLocation);
+                        GameRegistry.registerTileEntity(tileReference.loadClass().asSubclass(TileEntity.class), resourceLocation);
 
-                        if (registeredTiles.put(resourceLocation, tileReference.getClass().asSubclass(TileEntity.class)) != null)
+                        if (registeredTiles.put(resourceLocation, tileReference.loadClass().asSubclass(TileEntity.class)) != null)
                             LOGGER.error(String.format("The tile with ResourceLocation %s was already registered before.", resourceLocation.toString()));
                     });
+
+            classInfoList.filter(reference -> !reference.extendsSuperclass(TileEntity.class.getName()))
+                    .forEach(badReference -> LOGGER.error(
+                            String.format("The class %s annotated with %s was ignored because it is not a subclass of %s. ",
+                                    badReference.getName(), TileReference.class.getName(), TileEntity.class.getName())));
         }
     }
 
     // Fields >>
-    public static final Logger LOGGER = LogManager.getLogger("team_reborn|Scriba");
+    private final Logger LOGGER = LogManager.getLogger("team_reborn|Scriba");
 
     private final ConcurrentHashMap<ResourceLocation, Class<? extends TileEntity>> registeredTiles = new ConcurrentHashMap<>();
     // << Fields
