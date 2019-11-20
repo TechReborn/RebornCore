2d35d76b935383cc3844f8ace564b30db36aeb78
estebes <estebes@estebes.com>
2019-11-20 11:48:44 +0000

    Bump RebornCore version.

--- src/main/java/reborncore/RebornCore.java
+++ src/main/java/reborncore/RebornCore.java
@@ -65,7 +65,7 @@ public class RebornCore implements IModInfo {
 
 	public static final String MOD_NAME = "Reborn Core";
 	public static final String MOD_ID = "reborncore";
-	public static final String MOD_VERSION = "3.16.2"; //"@MODVERSION@";
+	public static final String MOD_VERSION = "@MODVERSION@";
 	public static final String WEB_URL = "https://files.modmuss50.me/";
 
 	public static LogHelper logHelper;
