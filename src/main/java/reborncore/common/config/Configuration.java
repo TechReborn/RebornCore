7b0c6b4e5f96b81c4c5c0389a529b6fc42443c99
modmuss50 <modmuss50@gmail.com>
2019-12-13 09:50:04 +0000

    Rewrite config

--- src/main/java/reborncore/common/config/Configuration.java
+++ src/main/java/reborncore/common/config/Configuration.java
@@ -28,13 +28,27 @@
 
 package reborncore.common.config;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
-import java.util.*;
+import java.lang.reflect.Modifier;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Configuration {
 
-	Class clazz;
-	String modId;
+	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+	private final Class clazz;
+	private final String modId;
 
 	public Configuration(Class clazz, String modId) {
 		this.clazz = clazz;
@@ -42,44 +56,136 @@ public class Configuration {
 		setup();
 	}
 
-	public static Map<String, ConfigurationWrapper> configMap = new HashMap<>();
-
 	private void setup() {
-		Arrays.stream(clazz.getDeclaredFields()).forEach(field -> handleField(field));
-		configMap.forEach((s, configuration) -> configuration.save());
+		final File configDir = new File(FabricLoader.getInstance().getConfigDirectory(), modId);
+
+		if (!configDir.exists()) {
+			configDir.mkdirs();
+		}
+
+		final File[] configFiles = configDir.listFiles();
+		if (configFiles != null) {
+			final HashMap<String, JsonObject> configs = new HashMap<>();
+			for (File file : configFiles) {
+				final String name = file.getName().substring(0, file.getName().length() - (".json".length()));
+				try {
+					final String fileContents = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+					final JsonObject jsonObject = GSON.fromJson(fileContents, JsonObject.class);
+					configs.put(name, jsonObject);
+				} catch (IOException e) {
+					System.err.println("Failed to read config file: " + file.getAbsolutePath());
+					e.printStackTrace();
+				}
+			}
+			readFromJson(configs);
+		}
+
+		//Save the configs
+		for (Map.Entry<String, JsonObject> entry : toJson().entrySet()) {
+			final File configFile = new File(configDir, entry.getKey() + ".json");
+			final String jsonStr = GSON.toJson(entry.getValue());
+			try {
+				FileUtils.writeStringToFile(configFile, jsonStr, StandardCharsets.UTF_8);
+			} catch (IOException e) {
+				throw new RuntimeException("Failed to write config file: " + configFile.getAbsolutePath(), e);
+			}
+		}
 	}
 
-	private void handleField(Field field){
-		if(!field.isAnnotationPresent(Config.class)){
-			return;
-		}
-		try {
+	private HashMap<Field, Config> getConfigFields() {
+		final HashMap<Field, Config> fieldMap = new HashMap<>();
+		for (Field field : clazz.getDeclaredFields()) {
+			if (!field.isAnnotationPresent(Config.class)) {
+				continue;
+			}
+			if (!Modifier.isStatic(field.getModifiers())) {
+				throw new UnsupportedOperationException("Config field must be static");
+			}
 			Config annotation = field.getAnnotation(Config.class);
+			fieldMap.put(field, annotation);
+		}
+		return fieldMap;
+	}
 
-			String key = annotation.key();
-			if(key.isEmpty()){
-				key = field.getName();
+	public HashMap<String, JsonObject> toJson() {
+		final HashMap<Field, Config> fieldMap = getConfigFields();
+		final HashMap<String, JsonObject> configs = new HashMap<>();
+
+		for (Map.Entry<Field, Config> entry : fieldMap.entrySet()) {
+			Field field = entry.getKey();
+			Config annotation = entry.getValue();
+
+			final JsonObject config = configs.computeIfAbsent(annotation.config(), s -> new JsonObject());
+
+			JsonObject categoryObject;
+			if (config.has(annotation.category())) {
+				categoryObject = config.getAsJsonObject(annotation.category());
+			} else {
+				categoryObject = new JsonObject();
+				config.add(annotation.category(), categoryObject);
 			}
 
-			String category = annotation.category();
-
-			ConfigurationWrapper configuration = configMap.get(modId);
-			if(configuration == null){
-				configuration = new ConfigurationWrapper(modId);
-				configMap.put(modId, configuration);
+			String key = annotation.key().isEmpty() ? field.getName() : annotation.key();
+			if (categoryObject.has(key)) {
+				throw new UnsupportedOperationException("Some bad happened, duplicate key found: " + key);
 			}
 
-			Object defaultValue = field.get(null);
+			JsonObject fieldObject = new JsonObject();
+			fieldObject.addProperty("comment", annotation.comment());
 
-			Object value = configuration
-				.getNode(category, key)
-				.setComment(annotation.comment())
-				.getValue(defaultValue);
+			Object value;
+			try {
+				value = field.get(null);
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
 
-			field.set(null, value);
+			JsonElement jsonElement = GSON.toJsonTree(value);
+			fieldObject.add("value", jsonElement);
 
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(e);
+			categoryObject.add(key, fieldObject);
+		}
+
+		return configs;
+	}
+
+	public void readFromJson(HashMap<String, JsonObject> configs) {
+		final HashMap<Field, Config> fieldMap = getConfigFields();
+
+		for (Map.Entry<Field, Config> entry : fieldMap.entrySet()) {
+			Field field = entry.getKey();
+			Config annotation = entry.getValue();
+
+			final JsonObject config = configs.get(annotation.config());
+
+			if (config == null) {
+				continue; //Could be possible if a new config is added
+			}
+
+			JsonObject categoryObject = config.getAsJsonObject(annotation.category());
+			if (categoryObject == null) {
+				continue;
+			}
+
+			String key = annotation.key().isEmpty() ? field.getName() : annotation.key();
+			if (!categoryObject.has(key)) {
+				continue;
+			}
+
+			JsonObject fieldObject = categoryObject.get(key).getAsJsonObject();
+			if (!fieldObject.has("value")) {
+				continue;
+			}
+			JsonElement jsonValue = fieldObject.get("value");
+			Class<?> fieldType = field.getType();
+
+			Object fieldValue = GSON.fromJson(jsonValue, fieldType);
+
+			try {
+				field.set(null, fieldValue);
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException("Failed to set field value", e);
+			}
 		}
 	}
 
